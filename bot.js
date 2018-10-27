7a89178f4478667b3de81e1f0dfedc0fdefb878f
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-10-27 12:20:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,6 +274,22 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    });
  
  
+
+
+ client.on('message', message => {
+	       var prefix = "1$";
+            if (message.content.startsWith(prefix + "bot")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات??',`[${client.guilds.size}]  `)
+.addField(' الاعضاء?? ',` [${client.users.size}] `)
+.addField('الرومات?? ',`[${client.channels.size}]`) 
+.addField(' البنق?? ',`[${Date.now() - message.createdTimestamp}]`) 
+.addField('مصمم  + صاحب البوت ',`! 1  raaxe`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
  
  
 client.login(process.env.BOT_TOKEN);
